58d45185330cf1613fa653041325f802851c70ae
Tommy <Tommy.ba95@gmail.com>
2023-07-06 17:30:42 +0200

    Add Home: Added base homepage design

--- my-portfolio/src/App.tsx
+++ my-portfolio/src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css';
 import { Header } from './components/header/Header';
 import { Home } from './components/home/Home';
+import { Projects } from './components/projects/Projects';
 
 function App() {
   return (
     <>
       <Header />
       <Home />
+      <Projects />
     </>
   );
 }
--- my-portfolio/src/components/home/Home.tsx
+++ my-portfolio/src/components/home/Home.tsx
@@ -1,8 +1,18 @@
+import "./Home.css";
+import * as BsIcon from "react-icons/bs";
+
 export const Home = () => {
     
         return (
             <div className="home-container">
-                <h1 className="home-title">Home</h1>
+                <div className="image-container__home">
+                    <BsIcon.BsPersonFill className="image__home" />
+                </div>
+                <div className="text-container__home">
+                    <h1 className="title__home">Hi, I'm <span className="name">Tommy</span></h1>
+                    <h2 className="sub-title__home">Full Stack Developer</h2>
+                    <p className="info-text__home">I'm a Full Stack Developer based in Oslo, Norway. I have a passion for web development and love to create for web and mobile devices.</p>
+                    </div>
             </div>
         )
 }
