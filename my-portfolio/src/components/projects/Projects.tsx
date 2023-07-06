5ae0dc8b92a3c087bbddb98e82ab0420dd1e840a
Tommy <Tommy.ba95@gmail.com>
2023-07-06 17:55:49 +0200

    Add Projects: added basic design for projects

--- my-portfolio/src/components/projects/Projects.tsx
+++ my-portfolio/src/components/projects/Projects.tsx
@@ -4,6 +4,16 @@ export const Projects = () => {
 
     return (
         <div className="projects-container">
+            <div className="project">
+                <h1 className="title__projects"> My Projects </h1>
+                <ul className="project-list">
+                    <li className="project-item">Project 1</li>
+                    <li className="project-item">Project 2</li>
+                    <li className="project-item">Project 3</li>
+                    <li className="project-item">Project 4</li>
+
+                </ul>
+            </div>
         </div>
     )
 }
